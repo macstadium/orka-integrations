0bd96fd3c78f51ef2e18f559cbbda7260b085867
Ivan Spasov <frankeys89@gmail.com>
2019-10-31 16:51:08 +0200

    Do not clean up

--- GitLab/scripts/cleanup.sh
+++ GitLab/scripts/cleanup.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 
-currentDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-source ${currentDir}/base.sh
-
-set -eo pipefail
-
-if [[ -f "$BUILD_ID" ]]; then
-    token=$(curl -sd '{"email":'\"$ORKA_USER\"', "password":'\"$ORKA_PASSWORD\"'}' -H "Content-Type: application/json" -X POST $ORKA_ENDPOINT/token | jq -r '.token')
-
-    vm_id=$(<$BUILD_ID)
-
-    curl -sd '{"orka_vm_name":'\"$vm_id\"'}' -H "Content-Type: application/json" -H "Authorization: Bearer $token" -X DELETE $ORKA_ENDPOINT/resources/vm/delete
-fi
+echo "Not cleaning up"
