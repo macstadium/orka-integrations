283326f0a019cb623e0a352ddfe6e7eec4f8a585
René de Hesselle <dehesselle@web.de>
2025-04-13 15:20:43 +0200

    Make the shell running the build a login shell
    This reflects the behavior we'd get from using the shell executor.
    Most prominently this is responsible for having PATH set up to
    include '/usr/local/bin' as a default.

--- GitLab/scripts/run.sh
+++ GitLab/scripts/run.sh
@@ -14,4 +14,4 @@ vm_ip=${info[0]}
 vm_ssh_port=${info[1]}
 
 ssh -i "$ORKA_SSH_KEY_FILE" \
-  -o ServerAliveInterval=60 -o ServerAliveCountMax=60 "$ORKA_VM_USER@$vm_ip" -p "$vm_ssh_port" /bin/bash < "${1}"
+  -o ServerAliveInterval=60 -o ServerAliveCountMax=60 "$ORKA_VM_USER@$vm_ip" -p "$vm_ssh_port" /bin/bash -l < "${1}"
